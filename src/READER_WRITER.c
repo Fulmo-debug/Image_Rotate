88a47b04fc929d93ff9a8b547a1edf7154965542
Fulmo-debug <77279691+Fulmo-debug@users.noreply.github.com>
2021-01-11 20:16:25 +0300

    fix fwrite

--- src/READER_WRITER.c
+++ src/READER_WRITER.c
@@ -26,10 +26,10 @@ enum read_status check_header(FILE* in, struct bmp_header* header) {
 
 
 enum read_status read_rgb(FILE* const in, struct bmp_header const header, struct pixel* const data_rgb){
-    uint32_t x = header.biWidth;
-    uint32_t y = header.biHeight;
+    uint64_t x = header.biWidth;
+    uint64_t y = header.biHeight;
     uint32_t indent = header.bOffBits;
-    int32_t padding = (4 - (x * sizeof(struct pixel) % 4)) % 4;
+    int64_t padding = (4 - (x * sizeof(struct pixel) % 4)) % 4;
     fseek(in, indent, SEEK_SET);
     for (size_t i = 0; i < y; i++){
         size_t collum = fread(data_rgb+x*i, sizeof(struct pixel), x, in);
@@ -44,8 +44,8 @@ enum read_status read_rgb(FILE* const in, struct bmp_header const header, struct
 enum read_status from_bmp(FILE* const in, struct image* img){
     struct bmp_header HEADER;
     enum read_status status_header = check_header(in, &HEADER);
-    uint32_t x = HEADER.biWidth;
-    uint32_t y = HEADER.biHeight;
+    uint64_t x = HEADER.biWidth;
+    uint64_t y = HEADER.biHeight;
     img->height = y;
     img->width = x;
     (*img).data = malloc(x*y*sizeof(struct pixel));
@@ -84,7 +84,7 @@ enum write_status to_bmp( FILE* const out, struct image const* img ){
     struct pixel* temp_data = img->data;
     uint64_t temp_x = img->width;
     uint64_t temp_y = img->height;
-    int32_t padding = (4 - (temp_x * sizeof(struct pixel) % 4)) % 4;
+    int64_t padding = (4 - (temp_x * sizeof(struct pixel) % 4)) % 4;
     struct bmp_header temp_header;
     create_bmp_header(&temp_header,img);
     size_t temp_value = fwrite(&temp_header, sizeof(struct bmp_header), 1, out);
@@ -94,8 +94,9 @@ enum write_status to_bmp( FILE* const out, struct image const* img ){
 
     for (size_t i = 0; i < temp_y; i++) {
         fwrite(&temp_data[temp_x * i], sizeof(struct pixel), temp_x, out);
-        fwrite(&padding, sizeof(padding), 1, out);
+        fwrite(&padding, 1, padding, out);
     }
     close_file(out);
+    free(temp_data);
     return WRITE_OK;
 }
