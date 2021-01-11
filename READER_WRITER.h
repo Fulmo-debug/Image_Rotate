d70aa97b2d5639d1b8a284404ffce0278c95c058
Fulmo-debug <77279691+Fulmo-debug@users.noreply.github.com>
2021-01-11 18:43:12 +0300

    Delete READER_WRITER.h

--- READER_WRITER.h
+++
@@ -1,8 +0,0 @@
-#include "stdio.h"
-#ifndef UNTITLED2_READER_WRITER_H
-#define UNTITLED2_READER_WRITER_H
-
-enum read_status from_bmp(FILE* const in, struct image* img);
-
-enum write_status to_bmp( FILE* const out, struct image const* img );
-#endif //UNTITLED2_READER_WRITER_H
