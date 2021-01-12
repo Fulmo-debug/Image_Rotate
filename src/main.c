09927f2e885b1259acf2c0bffaff33623d2afa13
Fulmo-debug <77279691+Fulmo-debug@users.noreply.github.com>
2021-01-12 15:04:14 +0300

    корректировка

--- src/main.c
+++ src/main.c
@@ -7,20 +7,20 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 3){
-        printf("Not enough arguments.\n");
-        printf("Check if you entered file paths.!\n");
+        printf(stderr,"Not enough arguments.\n");
+        fprintf(stderr,"Check if you entered file paths.!\n");
     }
 
     struct image temp_img;
     enum read_status temp_read_status = from_bmp(open_file_toREAD(argv[1]), &temp_img);
     if (temp_read_status){
-        printf("reading ERROR\n");
+        fprintf(stderr,"reading ERROR\n");
     }
 
     struct image rotate_img = rotate(temp_img);
     enum read_status temp_write_status = to_bmp(open_file_toWRITE(argv[2]), &rotate_img);
     if (temp_write_status){
-        printf("writing ERROR\n");
+        fprintf(stderr,"writing ERROR\n");
     }
     return 0;
 }
