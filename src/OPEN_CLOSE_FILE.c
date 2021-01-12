f5242a56b6d74fd0599f30f42fc6d083d7ae8053
Fulmo-debug <77279691+Fulmo-debug@users.noreply.github.com>
2021-01-12 14:58:28 +0300

    корректировка

--- src/OPEN_CLOSE_FILE.c
+++ src/OPEN_CLOSE_FILE.c
@@ -1,45 +1,29 @@
 #include <stdio.h>
 #include <stdint.h>
 
-/* Функция открывает файл   */
+/* Функция открывает файл для чтения   */
 FILE* open_file_toREAD(char const* file_name){
     FILE *in;
     in = fopen(file_name, "rb");
     if(in == NULL) {
-        printf("File open error");
-        printf("\n");
+    if(in == NULL) {
+        fprintf(stderr,"File open error\n");
     }
-    else{
-        printf("File opened successfully");
-        printf("\n");
-        return in;
-    }
+    return in;
 }
 
 /* Функция закрывает файл   */
 void close_file(FILE* in){
     int8_t status = fclose(in);
     if (status){
-        printf("File close error");
-        printf("\n");
-    }
-    else
-    {
-        printf("File closed successfully");
-        printf("\n");
+        fprintf(stderr,"File close error\n");
     }
 }
-
+/* Функция открывает файл для записи   */
 FILE* open_file_toWRITE(char const* file_name){
     FILE *out;
     out = fopen(file_name, "w+b");
     if(out == NULL){
-        printf("File open error");
-        printf("\n");
-    }
-    else{
-        printf("File opened successfully");
-        printf("\n");
-        return out;
+        fprintf(stderr,"File open error\n");
     }
 }
